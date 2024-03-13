2c8c40259b1b42f642c843a255e0b7e41b7e6746
Patrik <psirb@myuax.com>
2024-03-13 19:21:31 +0100

    las vistas pueden ser en interior tambien

--- src/main/java/SistemaReservasCafeteria/modelos/MesaDoble.java
+++ src/main/java/SistemaReservasCafeteria/modelos/MesaDoble.java
@@ -10,8 +10,8 @@ public class MesaDoble extends Mesa {
 
     public MesaDoble(int numero, String ubicacion, double precioReserva, LocalDate fechaUltimaReserva, String vista, int capacidad) {
         super(numero, ubicacion, precioReserva, fechaUltimaReserva);
-        if (!vista.equals("jardin") && !vista.equals("calle")) {
-            throw new IllegalArgumentException("La vista debe ser en jardin o calle");
+        if (!vista.equals("interior") && !vista.equals("jardin") && !vista.equals("calle")) {
+            throw new IllegalArgumentException("Las vistas elegibles son interior, jardin o calle");
         }
         this.vista = vista;
         this.capacidad = capacidad;
--- src/main/java/SistemaReservasCafeteria/modelos/MesaIndividual.java
+++ src/main/java/SistemaReservasCafeteria/modelos/MesaIndividual.java
@@ -10,8 +10,8 @@ public class MesaIndividual extends Mesa{
 
     public MesaIndividual(int numero, String ubicacion, double precioReserva, LocalDate fechaUltimaReserva, String vista, int capacidad) {
         super(numero, ubicacion, precioReserva, fechaUltimaReserva);
-        if (!vista.equals("jardin") && !vista.equals("calle")) {
-            throw new IllegalArgumentException("La vista debe ser en jardin o calle");
+        if (!vista.equals("interior") && !vista.equals("jardin") && !vista.equals("calle")) {
+            throw new IllegalArgumentException("Las vistas elegibles son interior, jardin o calle");
         }
         this.vista = vista;
         this.capacidad = capacidad;
