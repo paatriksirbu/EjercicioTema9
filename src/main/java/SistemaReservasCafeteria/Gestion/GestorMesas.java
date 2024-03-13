15c3263459712a4efd4840c53ab04eb496ee97ee
Patrik <psirb@myuax.com>
2024-03-13 19:09:32 +0100

    metodo eliminarMesas en el gestor de mesas.

--- src/main/java/SistemaReservasCafeteria/Gestion/GestorMesas.java
+++ src/main/java/SistemaReservasCafeteria/Gestion/GestorMesas.java
@@ -1,4 +1,23 @@
 package SistemaReservasCafeteria.Gestion;
+import SistemaReservasCafeteria.modelos.Mesa;
+
+import java.util.List;
 
 public class GestorMesas {
+
+    private List<Mesa> mesas;
+
+    public void agregarMesa(Mesa mesa){
+        mesas.add(mesa);
+    }
+
+    public void eliminarMesa(int numeroMesa){
+        mesas.removeIf(mesa -> mesa.getNumero() == numeroMesa);
+    }
+
+    public void mostrarMesas(){
+        for (Mesa mesa : mesas) {
+            System.out.println(mesa);
+        }
+    }
 }
