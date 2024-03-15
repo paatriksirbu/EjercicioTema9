91179fa3d795619852a201b29a7a9bc5e6f5d64b
Patrik <psirb@myuax.com>
2024-03-15 14:23:01 +0100

    metodos comunes de las mesas y para gestionar reservas de mesa.

--- src/main/java/SistemaReservasCafeteria/Interfaces/InterfazMesa.java
+++ src/main/java/SistemaReservasCafeteria/Interfaces/InterfazMesa.java
@@ -1,6 +1,14 @@
 package SistemaReservasCafeteria.Interfaces;
 
+import java.time.LocalDate;
+
 public interface InterfazMesa {
+    //Metodos para obtener los metodos comunes de las mesas
+    int getNumeroMesa();
+    String getUbicacion();
+    LocalDate getFechaUltimaReserva();
+
+    //Metodos para gestionar las reservas de mesas
     double getPrecioReserva();
     String mostrarInformacion();
 }
